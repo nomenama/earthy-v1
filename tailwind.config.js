de9218b841b775d0076324f4c092cabc0addde36
nomen <justnomen@outlook.com>
2022-12-16 11:19:53 +0000

    configure tailwind base and custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     theme: {
         extend: {
+            colors: {
+                primary: "#70c174",
+                secondary: "#bef3c0",
+                dark_01: "#0b0706",
+                dark_02: "#1d2130",
+                dark_03: "#525560",
+                dark_04: "#ebf0f9",
+                dark_05: "#e5e5e5",
+            },
             fontFamily: {
                 Roboto: ["Roboto", "sans-serif"],
             },
