3311a67abf6c752c08ecba26777579cd3c77e004
nomen <justnomen@outlook.com>
2022-12-16 20:46:56 +0000

    Added Footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="w-full bg-[#0b0706]">
             <div className="max-w-[90%] 3xl:max-w-[80%] mx-auto py-[50px] flex flex-col-reverse lg:flex-row justify-between lg:gap-[100px] gap-10">
                 <div className="flex-1 flex flex-col flex-wrap sm:flex-row sm:justify-between sm:items-start gap-[50px] sm:gap-6">
-                    <h4 className="text-white text-2xl text-center">Earthy</h4>
+                    <h4 className="text-white text-2xl text-center">Save the Earth</h4>
                     <div className="flex flex-col justify-start items-center text-white gap-6">
                         <p className="text-white font-bold">Our Team</p>
                         <Link
