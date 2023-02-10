f8251208cd37af86dfeb3a0c32eeb163de22aec9
nomen <justnomen@outlook.com>
2023-02-10 09:48:18 +0800

    small refactor

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
                 <section className="container bg-transparent py-[50px] flex flex-col gap-[50px]">
                     <Title title="Know About Us" />
 
-                    <div className="flex flex-row gap-[20px]">
+                    <div className="flex flex-row gap-[10px] ml-20">
                         <h1 className="w-1/2 text-heading_01 text-dark_02 font-bold leading-none">
                             We are a nonprofit team working worldwide
                         </h1>
@@ -137,7 +137,7 @@ const About = () => {
 
                 <div className="bg-dark_01 rounded-lg py-14 flex justify-end gap-10">
                     <div className="flex flex-col justify-center gap-4 ml-10">
-                        <h3 className="text-white uppercase leading-4 tracking-widest">
+                        <h3 className="text-white font-bold uppercase leading-4 tracking-widest">
                             Our Journey
                         </h3>
                         <h1 className="text-heading_01 font-bold text-white">How we raised 34M</h1>
